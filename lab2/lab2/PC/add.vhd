6c35f861f1dbd6e4cb0cb726cb11ae0dfcc215c7
nborhani20 <noushaborhani@gmail.com>
2026-03-29 18:25:38 -0400

    Add files via upload

---
+++ lab2/lab2/PC/add.vhd
@@ -0,0 +1,16 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_arith.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+entity add is
+port (
+		A : in std_logic_vector(31 downto 0);
+		B : out std_logic_vector(31 downto 0)
+		);
+end add;
+
+architecture Behaviour of add is
+begin
+	B <= A + 4;
+end Behaviour;
